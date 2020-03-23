35a9946d3a947b6c5da04c05cfb14a418bb0a1ab
Dylan Taft <dylbronjames@gmail.com>
2020-03-22 19:29:59 -0600

    added loader

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,6 +5,7 @@ import headshot from './headshot.png';
 const Home = () => {
   return (
     <section className="home" id="home">
+      <div class="ui active inline loader" id="loader"></div>
       <h1>Dylan Taft</h1>
       <p>Full Stack Developer</p>
       <img src={headshot} alt="headshot" className="headshot" />
